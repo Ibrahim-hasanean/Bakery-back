2e3e6e2360a29360aac729220d2840180ace6ae3
Ibrahim-hasanean <41992179+Ibrahim-hasanean@users.noreply.github.com>
2021-10-05 13:34:14 +0300

    fix users jwt secret env

--- src/middleware/verifyUser.ts
+++ src/middleware/verifyUser.ts
@@ -7,7 +7,7 @@ dotenv.config();
 export default async function verifyUser(req: IUserRequest, res: Response, next: NextFunction) {
     let authorization = req.headers.authorization;
     if (!authorization) return res.status(401).json({ status: 401, msg: "unauthorize" });
-    let jwtSecret = process.env.Admins_JWT_SECRET as string;
+    let jwtSecret = process.env.Users_JWT_SECRET as string;
     let token = authorization?.split(" ")[1];
     try {
         let decoded: any = jwt.verify(token, jwtSecret);
