5fcc1bd1a02e0da0027743a1ea396032cb3b695c
Ibrahim-hasanean <41992179+Ibrahim-hasanean@users.noreply.github.com>
2021-10-10 00:51:41 +0300

    set mongo url atlas

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -6,7 +6,7 @@ const databaseURL: string = process.env.DATABASE_URL as string;
 //     console.log("Error in database connection");
 //     console.log(e);
 // })
-mongoose.connect("mongodb://localhost:27017/el-madina").then(() => { console.log("database connected") }).catch((e) => {
+mongoose.connect("mongodb+srv://ibrahim:481997@cluster0.lyvgk.mongodb.net/bakeryTest?retryWrites=true&w=majority").then(() => { console.log("database connected") }).catch((e) => {
     console.log("Error in database connection");
     console.log(e);
 })
