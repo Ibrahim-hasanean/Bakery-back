287efef9d020fd7ea34f5eddae6bfe77c99af579
Ibrahim-hasanean <41992179+Ibrahim-hasanean@users.noreply.github.com>
2021-10-23 18:13:02 +0300

    change canManageManage to canManageUsers in users routes

--- src/routes/AdminUsers.ts
+++ src/routes/AdminUsers.ts
@@ -2,14 +2,15 @@ import { Router } from "express";
 import { validate, registerSchema } from "../middleware/userValidate";
 import { getUsers, addUser, deleteUser, getUser, upadateUser, toggleUserActive } from "../controllers/AdminUser";
 import verifyManager from "../middleware/verifyManager";
+import verifyCanManageUsers from "../middleware/verifyCanManageUsers";
 import verifyAdmin from "../middleware/verifyAdmin";
 const router = Router();
 
-router.get("/", verifyAdmin, getUsers);
-router.post("/", verifyManager, validate(registerSchema), addUser);
-router.delete("/:id", verifyManager, deleteUser);
-router.get("/:id", verifyAdmin, getUser);
-router.patch("/:id", verifyManager, upadateUser);
-router.get("/:id/active", verifyManager, toggleUserActive);
+router.get("/", verifyCanManageUsers, getUsers);
+router.post("/", verifyCanManageUsers, validate(registerSchema), addUser);
+router.delete("/:id", verifyCanManageUsers, deleteUser);
+router.get("/:id", verifyCanManageUsers, getUser);
+router.patch("/:id", verifyCanManageUsers, upadateUser);
+router.get("/:id/active", verifyCanManageUsers, toggleUserActive);
 
 export default router;
